cc62f4ae8ccd12f102b6e793b41100c95b56f221
mhheoSunyoutech <mhheo@sunyoutech.com>
2025-03-06 15:31:16 +0900

    Initial commit

---
+++ src/main/java/org/example/stocktracking/StockTrackingApplication.java
@@ -0,0 +1,13 @@
+package org.example.stocktracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/stocktracking/StockTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stocktracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
