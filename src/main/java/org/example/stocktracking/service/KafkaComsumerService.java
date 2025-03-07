e665b42ee92513f748ebbd1dadd4f7b4f31d0557
mhheoSunyoutech <mhheo@sunyoutech.com>
2025-03-07 16:15:56 +0900

    주식트래킹 백앤드 기초 셋팅

--- src/main/java/org/example/stocktracking/service/KafkaComsumerService.java
+++
@@ -1,9 +0,0 @@
-package org.example.stocktracking.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class KafkaComsumerService {
-
-    @
-}
---
+++ src/main/java/org/example/stocktracking/service/KafkaConsumerService.java
@@ -0,0 +1,14 @@
+package org.example.stocktracking.service;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "stockTracking")
+    public void listen(ConsumerRecord<?, ?> record) {
+        System.out.println(record.value());
+    }
+}
