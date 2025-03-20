7e028e9e5ac0d3da7d87e8cd3122da28fb1b5638
mhheoSunyoutech <mhheo@sunyoutech.com>
2025-03-20 13:28:12 +0900

    보안 강화

--- src/main/java/org/example/stocktracking/Controller/StockPriceApiClient.java
+++ src/main/java/org/example/stocktracking/Controller/StockPriceApiClient.java
@@ -4,6 +4,7 @@ import org.example.stocktracking.Dto.StockInfoDto;
 import org.example.stocktracking.Dto.StockInfoResponse;
 import org.example.stocktracking.Dto.StockPriceDto;
 import org.example.stocktracking.Dto.StockPriceResponse;
+import org.example.stocktracking.util.StockApiProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -16,21 +17,22 @@ public class StockPriceApiClient {
     private final WebClient webClient;
 
     private static final String BASE_URL = "http://data-dbg.krx.co.kr/svc/apis/sto/stk_bydd_trd";
-    private static final String AUTH_KEY = "38559DCA153E4D78B24872A40A414B3CD58F462B"; // 본인의 AUTH_KEY
+    private final StockApiProperties stockApiProperties;
 
-    public StockPriceApiClient(WebClient.Builder webClientBuilder) {
+    public StockPriceApiClient(WebClient.Builder webClientBuilder, StockApiProperties stockApiProperties) {
         this.webClient = webClientBuilder
                 .baseUrl(BASE_URL)
                 .exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10*1024*1024))
                         .build())
                 .build();
+        this.stockApiProperties = stockApiProperties;
     }
 
     public List<StockPriceDto> fetchStockPrice(String basDd) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder.queryParam("basDd", basDd).build())
-                .header("Authorization", AUTH_KEY)
+                .header("Authorization", stockApiProperties.getAuthKey("krx"))
                 .retrieve()
                 .bodyToMono(StockPriceResponse.class)  // JSON 응답을 Java 객체로 변환
                 .timeout(Duration.ofSeconds(10))  // ✅ 10초 타임아웃 설정
